a8370e2197285e01707eb2bbcab58539831ce254
Hannah Hendrickson <hannahhendrickson@Hannahs-MBP.PK5001Z>
2017-08-23 11:32:19 -0600

    Fixed getOrderInfo, passes

--- data-and-functions-1/src/functions/getOrderInfo.js
+++ data-and-functions-1/src/functions/getOrderInfo.js
@@ -2,27 +2,25 @@ import DATA from '../DATA'
 import getProductById from './getProductById'
 import getUserById from './getUserById'
 
-const getOrderInfo = (DATA, orderId) => {
-  if (DATA == null || DATA.users == null || orderId == null) {
+const getOrderInfo = (DATA) => {
+  if (DATA == null || DATA.orders == null) {
     return null
   }
-  let orderInfoArr = []
+  const orderInfoArr = []
 
   for (let i = 0; i < DATA.orders.length; i++) {
-    if (DATA.orders.id && DATA.orders.userId && DATA.orders.productId > 0) {
-      let orderId = DATA.orders.id
-      let userName = getUserById(DATA.orders.userId).name
-      let price = getProductById(DATA.order.productId).price
-      let orderInfo = {
-        orderId: orderId,
-        userName: userName,
-        price: price
-      }
-      orderInfoArr.push(orderInfo)
+    const currentOrder = DATA.orders[i]
+
+    const orderId = currentOrder.id
+    const userName = getUserById(DATA, currentOrder.userId).name
+    const price = getProductById(DATA, currentOrder.productId).price
+    const orderInfo = {
+      orderId: orderId,
+      userName: userName,
+      price: price
     }
-
+    orderInfoArr.push(orderInfo)
   }
-
   return orderInfoArr
 }
 
