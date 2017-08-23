de112605974f4d233876053b051d32014aa29802
davisgd <davis.diede@gmail.com>
2017-08-23 11:32:02 -0600

    added getOrderInfo

--- data-and-functions-1/src/functions/getOrderInfo.js
+++ data-and-functions-1/src/functions/getOrderInfo.js
@@ -1,15 +1,22 @@
-import DATA from '../DATA'
 import getUserById from './getUserById'
 import getProductById from './getProductById'
 
-const getOrderInfo = (id) => {
-  let orderArray = []
+const getOrderInfo = (DATA) => {
+  if (DATA == null || DATA.orders == null) {
+    return null
+  }
+  const orderArray = []
   DATA.orders.forEach((o) => {
-    orderArray.push({
-      userName: getUserById(o.userId).name,
-      orderId: o.id,
-      price: getProductById(o.productId).price
-    })
+    const currentOrder = DATA.orders[o]
+    const userName = getUserById(DATA, currentOrder.userId).name
+    const orderId = currentOrder.id
+    const price = getProductById(DATA, currentOrder.productId).price
+    const orderInfo = {
+      orderId: orderId,
+      userName: userName,
+      price: price
+    }
+    orderArray.push(orderInfo)
   })
   return orderArray
 }
--- data-and-functions-1/src/functions/getUserById.js
+++ data-and-functions-1/src/functions/getUserById.js
@@ -1,12 +1,10 @@
-import DATA from '../DATA'
-
-const getUserById = (userId) => {
+const getUserById = (DATA, userId) => {
   let user
   if (userId == null || DATA == null || DATA.users == null) {
     return null
   } else {
     DATA.users.forEach((u) => {
-      if (u.userId === id) {
+      if (u.userId === DATA.id) {
         user = u
       }
     })
