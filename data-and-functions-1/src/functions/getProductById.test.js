12fcf9ecb622c5c20ae086e374320f3b23aad095
Jay Cheroske <jay@portal.org>
2017-08-23 19:00:38 -0700

    fixing tests

--- data-and-functions-1/src/functions/getProductById.test.js
+++ data-and-functions-1/src/functions/getProductById.test.js
@@ -5,19 +5,25 @@ import getProductById from './getProductById'
 describe('getProductById', () => {
   it('is a function', () => expect(typeof getProductById).toBe('function'))
 
-  it('returns null with no id', () => expect(getProductById(DATA, null)).toBeNull())
+  it('returns null with null or undefined id', () => expect(getProductById(DATA, null)).toBeNull())
 
-  it('returns null with no data', () => expect(getProductById(null, 1)).toBeNull())
+  it('returns null with null or undefined data', () => expect(getProductById(null, 1)).toBeNull())
 
-  it('returns null with undefined products', () => expect(getProductById({}, 1)).toBeNull())
+  it('returns null with null or undefined data.products', () => expect(getProductById({}, 1)).toBeNull())
 
   it('returns an object when passed a good id', () => expect(typeof getProductById(DATA, 1)).toBe('object'))
 
-  it('return the correct result', () => {
+  it('returns the correct result', () => {
     expect(getProductById(DATA, 1)).toMatchObject({
       id: 1,
       name: 'Amazing Book',
       price: 30
+    })
+
+    expect(getProductById(DATA, 1)).toMatchObject({
+      id: 4,
+      name: 'Iron Chef Greatest Hits',
+      price: 60
     })
   })
 })
